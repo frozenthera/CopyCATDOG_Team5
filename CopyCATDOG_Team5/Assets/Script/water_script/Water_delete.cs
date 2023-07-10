32a8177e233a37f12a0d734751b800ebeb9697ad
hanchaehun <sweetkid01@naver.com>
2023-07-10 23:31:11 +0900

    .

--- CopyCATDOG_Team5/Assets/Script/water_script/Water_delete.cs
+++ CopyCATDOG_Team5/Assets/Script/water_script/Water_delete.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 
 public class Water_delete : MonoBehaviour
 {
     float timecount = 0;
     bool ones = true;
+    bool chgrid = true;
+    Vector2 pos;
+
+
 
     void Start()
     {
+        pos = this.transform.position;
         Destroy(gameObject, 2.3f); //≈Õ¡ˆ¥¬ ¿Ã∆—∆Æ 0.3√ 
-        
+
     }
 
     void Update()
@@ -25,7 +31,12 @@ public class Water_delete : MonoBehaviour
                 Debug.Log("π∞«≥º± ≈Õ¡¸");
                 ones = false;
 
-                
+                Debug.Log(pos);
+                Debug.Log(pos.x);
+                Debug.Log(pos.y);
+
+
+
             }
             
         }
