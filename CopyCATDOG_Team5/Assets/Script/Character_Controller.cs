ddf0f094fe32eef08ca75cb544648ae0155d6a5d
MSH03 <audtpgus12@naver.com>
2023-07-12 14:43:13 +0900

    두 캐릭터 이동

--- CopyCATDOG_Team5/Assets/Script/Character_Controller.cs
+++ CopyCATDOG_Team5/Assets/Script/Character_Controller.cs
@@ -5,81 +5,96 @@ using UnityEngine;
 
 public class Character_Controller : MonoBehaviour
 {
+    public bool FirstCharacter;
+    public Vector3 StartPosition;
+
     public int speed;
     private Rigidbody2D rigidbody;
     private Vector2 vector;
+
+
+    private KeyCode myKey1, myKey2, myKey3, myKey4;
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.position = new Vector3 (0, 0, 0);
+        transform.position = StartPosition;
         rigidbody = GetComponent<Rigidbody2D>();
+        if(FirstCharacter == true)
+        {
+            myKey1 = KeyCode.UpArrow;
+            myKey2 = KeyCode.DownArrow;
+            myKey3 = KeyCode.LeftArrow;
+            myKey4 = KeyCode.RightArrow;
+        }
+        else
+        {
+            myKey1 = KeyCode.W;
+            myKey2 = KeyCode.S;
+            myKey3 = KeyCode.A;
+            myKey4 = KeyCode.D;
+        }
     }
-
     // Update is called once per frame
     int temp;
     int active;
     void Update()
     {
         // 동시입력관리
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        if(Input.GetKeyDown(myKey1))
         {
             temp = 0;
             active = 1;
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(myKey2))
         {
             temp = 1;
             active = 1;
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(myKey3))
         {
             temp = 2;
             active = 1;
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(myKey4))
         {
             temp = 3;
             active = 1;
         }
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) && active == 1)
+        if (Input.GetKey(myKey1) || Input.GetKey(myKey2) || Input.GetKey(myKey3) || Input.GetKey(myKey4) && active == 1)
         {
             rigidbody.velocity = vector * speed;
             switch (temp)
             {
                 case 0:
-                    vector.y = 1;
-                    vector.x = 0;
+                    vector = Vector2.up;
                     break;
                 case 1:
-                    vector.y = -1;
-                    vector.x = 0;
+                    vector = Vector2.down;
                     break;
                 case 2:
-                    vector.x = -1;
-                    vector.y = 0;
+                    vector = Vector2.left;
                     break;
                 case 3:
-                    vector.x = 1;
-                    vector.y = 0;
+                    vector = Vector2.right;
                     break;
             }
         }
-        if (Input.GetKeyUp(KeyCode.UpArrow) && temp == 0)
+        if (Input.GetKeyUp(myKey1) && temp == 0)
         {
             active = 0;
             rigidbody.velocity = new Vector2(0, 0);
         }
-        if (Input.GetKeyUp(KeyCode.DownArrow) && temp == 1)
+        if (Input.GetKeyUp(myKey2) && temp == 1)
         {
             active = 0;
             rigidbody.velocity = new Vector2(0, 0);
         }
-        if (Input.GetKeyUp(KeyCode.LeftArrow) && temp == 2)
+        if (Input.GetKeyUp(myKey3) && temp == 2)
         {
             active = 0;
             rigidbody.velocity = new Vector2(0, 0);
         }
-        if (Input.GetKeyUp(KeyCode.RightArrow) && temp == 3)
+        if (Input.GetKeyUp(myKey4) && temp == 3)
         {
             active = 0;
             rigidbody.velocity = new Vector2(0, 0);
